9dd3728e7002f07c9e41a62d74655687401e9607
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-13 16:26:01 +0300

    users with  bank accounts

---
+++ oop_users_with_bank_accounts/assignment.py
@@ -0,0 +1,44 @@
+class BankAccount:
+    def __init__(self, int_rate, myBalance=0):
+        self.int_rate = int_rate
+        self.balance = myBalance
+
+    def deposit(self, myAmount):
+        self.balance += myAmount
+        return self
+
+    def withdraw(self, myAmount):
+        self.balance -= myAmount
+        return self
+
+    def display_account_info(self):
+        print(self.balance)
+        return self
+
+    def yield_intrest(self):
+        self.balance -= self.int_rate * self.balance
+        return self
+
+
+class User:
+    def __init__(self, myName, myEmail):
+        self.name = myName
+        self.email = myEmail
+        self.balance = BankAccount(int_rate=0, myBalance=0)
+
+    def make_deposit(self, myAmount):
+        self.balance.deposit(myAmount)
+        return self
+
+    def make_withdraw(self, myAmount):
+        self.balance.deposit(myAmount)
+        return self
+
+    def display_balance(self):
+        self.balance.display_account_info()
+        return self
+
+
+ahmad = User("Ahmad", "ahmad@gmail.com")
+
+print(ahmad.make_deposit(500))
