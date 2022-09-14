b882e0b115f3c7d5ddfcad028998b83d2a326e90
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-14 11:37:01 +0300

    upd

--- functions_intermediate_1/assignment.py
+++ functions_intermediate_1/assignment.py
@@ -15,5 +15,5 @@ print(randInt())  # should print a random integer between 0 to 100
 print(randInt(maxVal=50))  # should print a random integer between 0 to 50
 print(randInt(minVal=50))  # should print a random integer between 50 to 100
 print(randInt(minVal=50, maxVal=500))  # should print a random integer between 50 and 500
-print(randInt(maxVal=-1))
-print(randInt(minVal=50, maxVal=20))
+print(randInt(maxVal=-1)) #wrong parameters 
+print(randInt(minVal=50, maxVal=20)) #wrong parameters
