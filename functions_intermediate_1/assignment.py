1340c6502347e6bbaf94be2af36353df4e8661ac
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-14 12:37:48 +0300

    upd

--- functions_intermediate_1/assignment.py
+++ functions_intermediate_1/assignment.py
@@ -6,14 +6,16 @@ def randInt(minVal=0, maxVal=100):
     if maxVal < 0 or minVal > maxVal:
         return "Wrong Parameters"
     else:
-        num = random.random() * (maxVal - minVal) + minVal
-
+        num = ( random.random() * (maxVal - minVal) + minVal )  # its like mutliplying by percentage
+        # num = random.randint(minVal, maxVal)
     return int(num)
 
 
 print(randInt())  # should print a random integer between 0 to 100
 print(randInt(maxVal=50))  # should print a random integer between 0 to 50
 print(randInt(minVal=50))  # should print a random integer between 50 to 100
-print(randInt(minVal=50, maxVal=500))  # should print a random integer between 50 and 500
-print(randInt(maxVal=-1)) #wrong parameters 
-print(randInt(minVal=50, maxVal=20)) #wrong parameters
+print(
+    randInt(minVal=50, maxVal=500)
+)  # should print a random integer between 50 and 500
+print(randInt(maxVal=-1))  # wrong parameters
+print(randInt(minVal=50, maxVal=20))  # wrong parameters
